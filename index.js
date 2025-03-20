8770619ef60d65154c55a7c106589e7e0fab91bb
likayuni <148023197+likayuni@users.noreply.github.com>
2025-03-20 13:21:43 +0800

    mengubah file index.js

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 import http from "http";
+import mysql from "mysql2";
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "openapi",
+  password: "",
+});
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hewllo, World!");
+  db.query("SELECT * FROM user", (err, results) => {
+    if (err) {
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("Internal Server Eror");
+    }
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end(JSON.stringify(results));
+  });
 });
 server.listen(3000, () =>
-  console.log("Server berjalan di http://localhost:3000")
+  console.log("Server berjalan di http:/localhost:3000")
 );
